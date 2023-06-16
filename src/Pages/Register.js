e4c80f60450736eaf9e2b2cbaa120e851201f737
Prakash7078 <20bq1a05j5@vvit.net>
2023-06-16 20:55:18 +0530

    change register

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -10,44 +10,59 @@ import 'react-notifications/lib/notifications.css';
 function Register() {
   const[err,setErr]=useState(false);
   const navigate=useNavigate();
-  const handleSubmit=async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const displayName=e.target[0].value;
-    const email=e.target[1].value;
-    const password=e.target[2].value;
-    const file=e.target[3].files[0];
-    try{
-      const res=await createUserWithEmailAndPassword(auth,email,password);
-      const storageRef = ref(storage,displayName);
-
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        (error) => {
-          setErr(true);
-        }, 
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-            navigate("/login");
-            await updateProfile(res.user,{
-              displayName,
-              photoURL:downloadURL,
-            });
-            await setDoc(doc(db,"users",res.user.uid),{
-              uid:res.user.uid,
-              displayName,
-              email,
-              photoURL:downloadURL,
-            });
-            await setDoc(doc(db,"userChats",res.user.uid),{})
-            
-          });
-          
-        }
-      );
-    }catch(err){
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+  
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+  
+      await new Promise((resolve, reject) => {
+        const storageRef = ref(storage, displayName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+  
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {},
+          (error) => {
+            setErr(true);
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref)
+              .then(async (downloadURL) => {
+                await updateProfile(res.user, {
+                  displayName,
+                  photoURL: downloadURL,
+                });
+  
+                await setDoc(doc(db, "users", res.user.uid), {
+                  uid: res.user.uid,
+                  displayName,
+                  email,
+                  photoURL: downloadURL,
+                });
+                await setDoc(doc(db, "userChats", res.user.uid), {});
+  
+                resolve();
+              })
+              .catch((error) => {
+                setErr(true);
+                reject(error);
+              });
+          }
+        );
+      });
+  
+      navigate("/login");
+    } catch (err) {
       setErr(true);
     }
-  }
+  };
+  
   return (
     <div className='formContainer'>
         <div className="formWrapper">
@@ -57,11 +72,7 @@ function Register() {
                 <input type='text' placeholder='display name'/>
                 <input type='email' placeholder='email'/>
                 <input type='password' placeholder='password'/>
-                <input style={{display:"none"}} type='file' id="file"/>
-                <label htmlFor='file'>
-                    <MdOutlineAddPhotoAlternate size={32}/>
-                    <span>Add Avatar</span>
-                </label>
+                <input  type='file' id="file"/>
                 <button>Sign Up</button>
                 {err && NotificationManager.info("loading...")}
                 <p>already have an account? <Link to="/login">Login</Link></p>
